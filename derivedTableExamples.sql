99ac9fbe854c6ceabb5b443fbc8b89f032296cd1
mlambek <maxwell.lambek@colorado.edu>
2016-10-30 12:45:24 -0600

    added .sql files and schema screenshot

---
+++ derivedTableExamples.sql
@@ -0,0 +1,3 @@
+CREATE TABLE derived102916Halloween (geotag GEOMETRY, date DATE, coords TEXT, weight INT);
+CREATE TABLE derived102916outdoors (geotag GEOMETRY, date DATE, coords TEXT, weight INT);
+CREATE TABLE derived102916WASvsCIN (geotag GEOMETRY, date DATE, coords TEXT, weight INT);
---
+++ largeTable.sql
@@ -0,0 +1,1 @@
+CREATE TABLE largeTable (hashtag TEXT, coords TEXT, geotag GEOMETRY, date DATE);
